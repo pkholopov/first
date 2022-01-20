3b9897d0617230265b2cf6952a71c0d364c25c8b
PKholopov <ironface13@gmail.com>
2022-01-21 01:00:20 +0300

    minor fixes

--- dynamic_content.js
+++ dynamic_content.js
@@ -10,7 +10,7 @@ const noImage = "no_image.jpg";
 //     this.temp = temp;
 // }
 // let mercury = new CelestialBody('planet','Меркурий','https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg','');
-const solarSysterm = [
+const solarSystem = [
     {
         type:'planet',
         name:'Меркурий',
@@ -63,17 +63,17 @@ const solarSysterm = [
 const contentTable = document.querySelector('.content_table');
 for (let i = 0; i < solarSysterm.length; i++) {
     const template = `<div class="content_cell">
-    <div class="cell_image">
-        <img src="${solarSysterm[i].image}" />
-    </div>
-    <div class="cell_info">
-        <p><b>Название: </b>${solarSysterm[i].name}</p>
-        <p><b>Большая полуось орбиты: </b>${solarSysterm[i].smAxis} км</p>
-        <p><b>Средний радиус: </b>${solarSysterm[i].aRadius} км</p>
-        <p><b>Масса: </b>${solarSysterm[i].mass.wholePart}<sup>${solarSysterm[i].mass.degree}</sup> кг</p>
-        <p><b>Температура: </b>${solarSysterm[i].temp}&deg;C</p>
-    </div>
-</div>`
+                        <div class="cell_image">
+                            <img src="${solarSystem[i].image}" />
+                        </div>
+                        <div class="cell_info">
+                            <p><b>Название: </b>${solarSystem[i].name}</p>
+                            <p><b>Большая полуось орбиты: </b>${solarSystem[i].smAxis} км</p>
+                            <p><b>Средний радиус: </b>${solarSystem[i].aRadius} км</p>
+                            <p><b>Масса: </b>${solarSystem[i].mass.wholePart}<sup>${solarSystem[i].mass.degree}</sup> кг</p>
+                            <p><b>Температура: </b>${solarSystem[i].temp}&deg;C</p>
+                        </div>
+                    </div>`
     contentTable.innerHTML+=template;
-    
+
 }
